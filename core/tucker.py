286bd5741947b85ed3b42c09d2b58ca2635a596a
rakhuba <m.rakhuba@skoltech.ru>
2017-05-31 11:21:52 +0200

    mkl_fft fix

--- core/tucker.py
+++ core/tucker.py
@@ -3,7 +3,7 @@ import time
 from math import pi
 import maxvol as mv
 import copy
-import mkl_fft
+#import mkl_fft
 import scipy.interpolate as interpolate
 
 def svd(A):
@@ -54,16 +54,16 @@ class tensor:
 
     def __getitem__(self, index):
         return [self.u[0], self.u[1], self.u[2], self.core][index]
-        
-    
+
+
     def __repr__(self):
-        
+
         res = "This is a 3D tensor in the Tucker format with \n"
         r = self.r
         n = self.n
         for i in xrange(3):
             res = res + ("r(%d)=%d, n(%d)=%d \n" % (i, r[i], i, n[i]))
-        
+
         return res
 
     def __add__(self, other):
@@ -71,19 +71,19 @@ class tensor:
         #if self.n != other.n:
         #print self.n, other.n
         #  raise Exception('mode sizes must agree')
-        
+
         c = tensor()
         c.r = [self.r[0] + other.r[0], self.r[1] + other.r[1], self.r[2] + other.r[2] ]
         c.n = self.n
         c.u[0] = np.concatenate((self.u[0], other.u[0]), axis = 1)
         c.u[1] = np.concatenate((self.u[1], other.u[1]), axis = 1)
         c.u[2] = np.concatenate((self.u[2], other.u[2]), axis = 1)
-        
+
         if type(self.core[0,0,0]*other.core[0,0,0]*self.u[0][0,0]*other.u[0][0,0]) is np.complex128:
             dtype = np.complex128
         else:
             dtype = np.float64
-                
+
         c.core = np.zeros(c.r, dtype=dtype)
         c.core[:self.r[0], :self.r[1], :self.r[2]] = self.core
         c.core[self.r[0]:, self.r[1]:, self.r[2]:] = other.core
@@ -106,8 +106,8 @@ class tensor:
         b.core = (-1.) * b.core
         sub = a + b
         return sub
-        
-                
+
+
     def full(self):
 
         A = np.tensordot(self.core, np.transpose(self.u[2]), (2,0))
@@ -122,14 +122,14 @@ class tensor:
 def can2tuck(g, U1, U2, U3):
 
     a = tensor()
-    
+
     n, r1 = U1.shape
     n, r2 = U2.shape
     n, r3 = U3.shape
 
     if r1<>r2 or r2<>r3 or r1<>r3:
         raise Exception("Wrong factor sizes")
-    
+
     r = r1
     if type(g[0]) is np.complex128:
         G = np.zeros((r, r, r), dtype = np.complex128)
@@ -140,18 +140,18 @@ def can2tuck(g, U1, U2, U3):
 
     a.r = (r1, r2, r3)
     a.n = (n, n, n)
-    
+
     a.core = G
     a.u[0] = U1.copy()
     a.u[1] = U2.copy()
     a.u[2] = U3.copy()
-    
+
     return a
 
 def real(a): # doubled ranks!
 
     b = tensor()
-    
+
     b.n = a.n
     b.u[0] = np.concatenate((np.real(a.u[0]), np.imag(a.u[0])), 1)
     b.u[1] = np.concatenate((np.real(a.u[1]), np.imag(a.u[1])), 1)
@@ -167,17 +167,17 @@ def real(a): # doubled ranks!
     R2[a.r[1]:, :] = 1j*np.identity(a.r[1])
     R3[:a.r[2], :] = np.identity(a.r[2])
     R3[a.r[2]:, :] = 1j*np.identity(a.r[2])
-    
-    
+
+
     GG = np.tensordot(np.transpose(a.core,[2,1,0]),np.transpose(R1), (2,0))
     GG = np.tensordot(np.transpose(GG,[0,2,1]),np.transpose(R2), (2,0))
     GG = np.transpose(GG,[1,2,0])
     b.core = np.real(np.tensordot(GG,np.transpose(R3), (2,0)))
 
     b.r = b.core.shape
-    
+
     return b
-    
+
 def full(a, ind = None):
     if ind == None:
         return a.full()
@@ -200,7 +200,7 @@ def qr(a):
     b.core = a.core
     b.r = a.r
     b.n = a.n
-    
+
     b.u[0], R1 = np.linalg.qr(a.u[0])
     b.u[1], R2 = np.linalg.qr(a.u[1])
     b.u[2], R3 = np.linalg.qr(a.u[2])
@@ -263,16 +263,16 @@ def fft(a):
     b.r = a.r
     b.n = a.n
 
-    try:
-        b.u[0] = mkl_fft1d(a.u[0])
-        b.u[1] = mkl_fft1d(a.u[1])
-        b.u[2] = mkl_fft1d(a.u[2])
-
-    except:
-        print 'Standard np.fft.fft operation. May be slow if it is not from mkl'
-        b.u[0] = np.fft.fft(a[0], axis = 0)
-        b.u[1] = np.fft.fft(a[1], axis = 0)
-        b.u[2] = np.fft.fft(a[2], axis = 0)
+    # try:
+    #     b.u[0] = mkl_fft1d(a.u[0])
+    #     b.u[1] = mkl_fft1d(a.u[1])
+    #     b.u[2] = mkl_fft1d(a.u[2])
+    #
+    # except:
+    #     print 'Standard np.fft.fft operation. May be slow if it is not from mkl'
+    b.u[0] = np.fft.fft(a[0], axis = 0)
+    b.u[1] = np.fft.fft(a[1], axis = 0)
+    b.u[2] = np.fft.fft(a[2], axis = 0)
 
 
 
@@ -286,23 +286,23 @@ def ifft(a):
     b.r = a.r
     b.n = a.n
 
-    try:
-        b.u[0] = mkl_ifft1d(a.u[0])
-        b.u[1] = mkl_ifft1d(a.u[1])
-        b.u[2] = mkl_ifft1d(a.u[2])
-    
-    except:
-        print 'Standard np.fft.fft operation. May be slow if it is not from mkl'
-        b.u[0] = np.fft.ifft(a[0], axis = 0)
-        b.u[1] = np.fft.ifft(a[1], axis = 0)
-        b.u[2] = np.fft.ifft(a[2], axis = 0)
+    # try:
+    #     b.u[0] = mkl_ifft1d(a.u[0])
+    #     b.u[1] = mkl_ifft1d(a.u[1])
+    #     b.u[2] = mkl_ifft1d(a.u[2])
+    #
+    # except:
+    #     print 'Standard np.fft.fft operation. May be slow if it is not from mkl'
+    b.u[0] = np.fft.ifft(a[0], axis = 0)
+    b.u[1] = np.fft.ifft(a[1], axis = 0)
+    b.u[2] = np.fft.ifft(a[2], axis = 0)
 
 
 
     return b
 
 def dst(a):
-    
+
     b = tensor()
 
     b.core = a.core
@@ -312,7 +312,7 @@ def dst(a):
         b.u[0] = dst1D(np.real(a[0])) + 1j * dst1D(np.imag(a[0]))
         b.u[1] = dst1D(np.real(a[1])) + 1j * dst1D(np.imag(a[1]))
         b.u[2] = dst1D(np.real(a[2])) + 1j * dst1D(np.imag(a[2]))
-    
+
     else:
         b.u[0] = dst1D(np.real(a[0]))
         b.u[1] = dst1D(np.real(a[1]))
@@ -321,7 +321,7 @@ def dst(a):
     return b
 
 def idst(a):
-    
+
     b = tensor()
 
     b.core = a.core
@@ -331,7 +331,7 @@ def idst(a):
         b.u[0] = idst1D(np.real(a[0])) + 1j * idst1D(np.imag(a[0]))
         b.u[1] = idst1D(np.real(a[1])) + 1j * idst1D(np.imag(a[1]))
         b.u[2] = idst1D(np.real(a[2])) + 1j * idst1D(np.imag(a[2]))
-    
+
     else:
         b.u[0] = idst1D(np.real(a[0]))
         b.u[1] = idst1D(np.real(a[1]))
@@ -340,16 +340,16 @@ def idst(a):
     return b
 
 def svd_trunc(A, eps = 1e-14):
-    
+
     u, s, v = svd(A)
 
     N1, N2 = A.shape
-    
+
     eps_svd = eps*s[0]/np.sqrt(3)
     r = min(N1, N2)
     for i in xrange(min(N1, N2)):
         if s[i] <= eps_svd:
-            r = i          
+            r = i
             break
         #print s/s[0]
     u = u[:,:r].copy()
@@ -364,26 +364,26 @@ def H(A):
 
 def ones((n1, n2, n3), dtype = np.float64):
     a = tensor()
-    
+
     a.u[0] = np.ones((n1, 1), dtype = dtype)
     a.u[1] = np.ones((n2, 1), dtype = dtype)
     a.u[2] = np.ones((n3, 1), dtype = dtype)
     a.core = np.ones((1, 1, 1), dtype = dtype)
     a.r = (1, 1, 1)
     a.n = (n1, n2, n3)
-    
+
     return a
 
 def zeros((n1, n2, n3), dtype = np.float64):
     a = tensor()
-    
+
     a.u[0] = np.zeros((n1, 1), dtype = dtype)
     a.u[1] = np.zeros((n2, 1), dtype = dtype)
     a.u[2] = np.zeros((n3, 1), dtype = dtype)
     a.core = np.ones((1, 1, 1), dtype = dtype)
     a.r = (1, 1, 1)
     a.n = (n1, n2, n3)
-    
+
     return a
 
 
@@ -395,7 +395,7 @@ def dst1D(A):
     X = np.zeros(new_size, dtype = np.complex128)
 
     X[1: n[0] + 1, :] = A
-    X = np.imag(mkl_fft1d(X))
+    X = np.imag(np.fft.fft(X, axis=0))
     return -X[1: n[0] + 1, :] * np.sqrt(2./(n[0] + 1))
 
 def idst1D(A):
@@ -406,7 +406,7 @@ def idst1D(A):
     X = np.zeros(new_size, dtype = np.complex128)
 
     X[1: n[0] + 1, :] = A
-    X = (2*(n[0]+1))* np.imag(mkl_ifft1d(X))
+    X = (2*(n[0]+1))* np.imag(np.fft.ifft(X, axis=0))
     return X[1: n[0] + 1, :] * np.sqrt(2./(n[0] + 1))
 
 def dst3D(A):
@@ -419,44 +419,37 @@ def dst3D(A):
     return X
 
 def mkl_fft1d(a):
-    
+
     n,m = a.shape
     b = np.zeros((n,m),dtype=np.complex128)
     for i in xrange(m):
-        b[:,i] = mkl_fft.fft(a[:,i]+0j)
+        b[:,i] = np.fft.fft(a[:,i]+0j, axis=0)
     return b
 
 def mkl_ifft1d(a):
-    
+
     n,m = a.shape
     b = np.zeros((n,m),dtype=np.complex128)
     for i in xrange(m):
-        b[:,i] = mkl_fft.ifft(a[:,i]+0j)/n
+        b[:,i] = np.fft.ifft(a[:,i]+0j, axis=0)/n
     return b
 
 
 def interp(a, x_old, x_new):
-    
+
     b = copy.deepcopy(a)
     n_new = len(x_new)
     b.n = [n_new]*3
-    
+
     b.u[0] = np.zeros((n_new, b.r[0]), dtype = type(a.u[0][0,0]))
     b.u[1] = np.zeros((n_new, b.r[1]), dtype = type(a.u[1][0,0]))
     b.u[2] = np.zeros((n_new, b.r[2]), dtype = type(a.u[2][0,0]))
-    
-    
+
+
     for alpha in xrange(3):
         for i in xrange(a.r[alpha]):
             temp = copy.copy(a.u[alpha][:, i])
             tck = interpolate.splrep(x_old, temp, s=0)
             b.u[alpha][:, i] = interpolate.splev(x_new, tck, der=0)
-    
+
     return b
-
-
-
-
-
-
-
